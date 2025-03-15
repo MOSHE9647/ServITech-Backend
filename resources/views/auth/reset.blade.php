566d257a910bdf358cbaa538dde85018ed479eeb
Moshe9647 <isaacmhp2001@gmail.com>
2025-03-15 16:24:20 -0600

    Actualizar la vista de restablecimiento de contraseña: corregir la visualización de errores para los campos de contraseña y confirmación de contraseña.

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -36,7 +36,7 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required>
                         @error('password')
-                            <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+                            <p class="text-red-500 text-xs italic mt-2">{{ $errors->first('password') }}</p>
                         @enderror
                     </div>
                     <div>
@@ -47,8 +47,8 @@
                         <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required>
-                        @error('password_confirmation')
-                            <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+                        @error('password')
+                            <p class="text-red-500 text-xs italic mt-2">{{ $errors->first('password_confirmation') }}</p>
                         @enderror
                     </div>
                     <button type="submit"
