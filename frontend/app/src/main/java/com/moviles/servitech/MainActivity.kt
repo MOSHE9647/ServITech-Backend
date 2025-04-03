c30a4690e5f47a7a0ae34a370e3889d4c5f00a36
Moshe9647 <isaacmhp2001@gmail.com>
2025-04-02 18:22:09 -0600

    Reorganiza la estructura del proyecto en backend y frontend; agrega proyecto de Android Studio al frontend con configuración inicial

---
+++ frontend/app/src/main/java/com/moviles/servitech/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.moviles.servitech
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moviles.servitech.ui.theme.ServITechTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ServITechTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ServITechTheme {
+        Greeting("Android")
+    }
+}
