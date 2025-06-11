3cb32bf41e026204c771c8a7c5afa091779122ed
Moshe9647 <isaacmhp2001@gmail.com>
2025-06-10 21:52:03 -0600

    update: Adds 'id' field to RepairRequestResource response

--- app/Http/Resources/RepairRequestResource.php
+++ app/Http/Resources/RepairRequestResource.php
@@ -15,6 +15,7 @@ class RepairRequestResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'                   => $this->id,
             'receipt_number'       => $this->receipt_number,
             'customer_name'        => $this->customer_name,
             'customer_phone'       => $this->customer_phone,
